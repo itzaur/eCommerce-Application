6e7518cae4dc140925d0c29f4574725eb4c547a2
Oksana <ksu1ven@mail.ru>
2023-08-05 16:44:24 +0300

    chore: configure jest and work with react

--- eCommerce-Application/src/App.tsx
+++ eCommerce-Application/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import viteLogo from '/vite.svg';
+// import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -10,7 +10,7 @@ function App() {
         <>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                    {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
                 </a>
                 <a href="https://react.dev" target="_blank">
                     <img
